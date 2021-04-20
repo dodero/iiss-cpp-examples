0612ff2669007aa66e153ff35f2383a2b644037c
GonzaloUli <43575107+gonzalouli@users.noreply.github.com>
2021-04-20 12:47:45 +0200

    Add files via upload

--- temas/promesas/promise.cpp
+++ temas/promesas/promise.cpp
@@ -8,20 +8,20 @@
 #include <string> 
 #include <thread>
 
-
 using namespace std;
 
 int functionFactorial ( std::future<int>& promiseOfFuture ){
 	
 	int finalFactorial =1;
-	int numberFactorial = promiseOfFuture.get(); //Hasta que no se ejecute el set_value del hilo principal la promesa de un futuro no continuara
+	int numberFactorial = promiseOfFuture.get(); //Hasta que no se ejecute el 
+						//set_value del hilo principal la promesa de un futuro no continuara
 	
 	for(int nextfactor= numberFactorial ; nextfactor>1 ; nextfactor--) //Calculo de factorial
 		finalFactorial *=nextfactor;
 		
 	cout << "El factorial es "<< finalFactorial << endl;
 	
-	return finalFactorial; //Devolucion de un valor futuro (finalfactorial)
+	return finalFactorial; //Devolución de un valor futuro (finalfactorial)
 }
 
 int main(){
@@ -30,25 +30,33 @@ int main(){
 	int factorial; //Factorial final
 	
 	std::promise<int> promise;	//Promesa de un valor
-	std::future<int> promiseOfFuture = promise.get_future(); //La primesa nos proporcionarï¿½ un futuro
-    std::future<int> future = std::async(std::launch::async , functionFactorial, std::ref(promiseOfFuture));  //Ejecucion asincrona
+	std::future<int> promiseOfFuture = promise.get_future(); //La promesa nos proporcionará un futuro
+    std::future<int> future = std::async(std::launch::async , 
+			functionFactorial, std::ref(promiseOfFuture));  //Ejecución asincrona
     
     std::this_thread::sleep_for(chrono::milliseconds(10)); //dormimos el hilo principal por un tiempo
 	
 
     try{
 
-     promise.set_value(factor); //Damos el valor prometido anteriormente a promise.
-	   factorial = future.get(); //Obtenemos el valor prometido del futuro del hilo asincrono que ejecuta facotiral
-    
-     cout << factor << "! = "<<factorial ; //Mostramos el factorial del factor 'factor'.
+    promise.set_value(factor); //Damos el valor prometido anteriormente a promise.
+	factorial = future.get(); //Obtenemos el valor prometido del futuro 
+								//del hilo asincrono que ejecuta factorial
+    cout << factor << "! = "<<factorial ; //Mostramos el factorial del factor 'factor'.
 	
-	  }catch(std::exception &e){ //Si se ejecuta una excepcion y la lanzamos.
-		  promise.set_exception(std::current_exception());
-	  }finally{
+	}catch(std::exception &e){ //Si ocurre una excepcion, la lanzamos.
+		promise.set_exception(std::current_exception());
+	}finally{
 	    return 0;
 	}
     
 }
+
+
+
+
+
+
+
 
 
