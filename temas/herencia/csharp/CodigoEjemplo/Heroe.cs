3fd6428a7e8d4f3a770019d6dfaac147969dd049
María Vázquez Sáez <mariav.s.00@gmail.com>
2022-05-09 20:24:44 +0200

    Codigo ejemplo herencia csharp

---
+++ temas/herencia/csharp/CodigoEjemplo/Heroe.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace Aventura
+{
+    public class Heroe : PersonajeDeAccion, SabeLuchar, SabeNadar,SabeVolar
+    { 
+        public void volar() { Console.WriteLine("SABE VOLAR HEROE"); }
+
+        public void nadar() { Console.WriteLine("SABE NADAR HEROE"); }
+    }
+}
